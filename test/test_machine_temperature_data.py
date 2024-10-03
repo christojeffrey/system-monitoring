5908d6eb79f7242124459d252e54dd0a8c79e001
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-10-04 02:39:05 +0700

    test: add test

---
+++ test/test_machine_temperature_data.py
@@ -0,0 +1,49 @@
+import unittest
+import pandas as pd
+from src.machine_temperature_data.data import MachineTemperatureData
+
+class TestMachineTemperatureData(unittest.TestCase):
+    def setUp(self):
+        self.mtd = MachineTemperatureData(
+            MACHINE_MIN_TEMPERATURE=20,
+            MACHINE_MAX_TEMPERATURE=80,
+            NOISE_RANGE=2,
+            NOISE_PROBABILITY=0.1,
+            WORKING_FLIP_PROBABILITY=0.05
+        )
+
+    def test_generate_next_data(self):
+        initial_length = self.mtd.getDataLength()
+        self.mtd.data = self.mtd.generateNextData()
+        self.assertEqual(self.mtd.getDataLength(), initial_length + 1)
+
+    def test_limit_data(self):
+        for _ in range(100):
+            self.mtd.data = self.mtd.generateNextData()
+        self.mtd.limitData(30)
+        self.assertEqual(self.mtd.getDataLength(), 30)
+
+    def test_set_last_data_as_anomaly(self):
+        self.mtd.setLastDataAsAnomaly()
+        self.assertTrue(self.mtd.getAnomalyData()[-1], True)
+
+    def test_get_data_index(self):
+        index = self.mtd.getDataIndex()
+        self.assertIsInstance(index, pd.DatetimeIndex)
+
+    def test_get_temperature_data(self):
+        temp_data = self.mtd.getTemperatureData()
+        self.assertIsInstance(temp_data, pd.Series)
+
+    def test_get_anomaly_data(self):
+        self.mtd.setLastDataAsAnomaly()
+        anomaly_data = self.mtd.getAnomalyData()
+        self.assertEqual(len(anomaly_data), 1)
+
+    def test_get_last_temperature(self):
+        last_temp = self.mtd.getLastTemperature()
+        self.assertIsInstance(last_temp, float)
+        self.assertTrue(20 <= last_temp <= 80)
+
+if __name__ == '__main__':
+    unittest.main()
